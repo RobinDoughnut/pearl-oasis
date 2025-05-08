a0b56dd75102eb228c42077be157cedaedfc6e56
Taasin Alvi <77426735+RobinDoughnut@users.noreply.github.com>
2025-05-08 22:59:16 +0600

    meta tag added

--- php/index.php
+++ php/index.php
@@ -12,11 +12,12 @@ $script_path = BASE_PATH . "assets/js/main.js";
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
+    <meta name="keywords" content="jewelry, gold, silver, gemstone, handmade, custom, Bangladesh">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>
-        Home | Pearl Oasis
+        Home | Pearl Oasis 
     </title>
     <meta name="description"
         content="Shop premium gold, silver, and gemstone jewelry. Handmade elegance, crafted in Bangladesh. Custom orders available.">
